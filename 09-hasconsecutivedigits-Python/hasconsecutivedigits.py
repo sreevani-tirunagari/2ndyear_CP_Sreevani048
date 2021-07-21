4843d2b72ce2330078598240660e3cf729186518
“sreevani-tirunagari” <72122307+sreevani-tirunagari@users.noreply.github.com>
2021-07-21 22:37:01 +0530

    code done

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -3,5 +3,13 @@
 # number contains two consecutive digits that are the same, and False otherwise.
 
 def hasconsecutivedigits(n):
-	# your code goes here
-	pass
+	x = [int(i) for i in str(abs(n))]
+	a = []
+	for i in x:
+		count = x.count(i)
+		if (count>1):
+			a.append(i)
+	if (len(a)==0):
+		return False
+	else:
+		return True
