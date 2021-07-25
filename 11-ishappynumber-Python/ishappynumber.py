03f279f3788530665432eb3b284cc384f44970e6
“sreevani-tirunagari” <72122307+sreevani-tirunagari@users.noreply.github.com>
2021-07-25 19:00:52 +0530

    code done

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -17,6 +17,27 @@
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
 
+def numSquareSum(n): 
+    squareSum = 0; 
+    while(n): 
+        squareSum += (n % 10) * (n % 10); 
+        n = int(n / 10); 
+    return squareSum; 
+
+
 def ishappynumber(n):
-	# your code goes here
-	pass
+    slow = n; 
+    fast = n; 
+    while(True): 
+          
+    
+   
+        slow = numSquareSum(slow); 
+
+        fast = numSquareSum(numSquareSum(fast)); 
+        if(slow != fast): 
+            continue; 
+        else: 
+            break; 
+  
+    return (slow == 1); 
