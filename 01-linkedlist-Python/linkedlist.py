eb355a20ea690e0a9e425d45bbad66ea92fe213d
“sreevani-tirunagari” <72122307+sreevani-tirunagari@users.noreply.github.com>
2021-08-02 00:43:09 +0530

    code done

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -19,14 +19,30 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
+        #pass
+        n=self.head
+        while n.next!=None:
+            n=n.next
+        n.next=new_element
+     
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        #pass
+        counter=1
+        n=self.head
+        if position<1:
+            return None
+
+        while n and counter <= position:
+            if counter==position:
+                return n
+            n=n.next
+            counter = counter+1
+        return None
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,10 +50,32 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
+        #pass
+        counter=1
+        n=self.head
+        if position>1:
+            while n and counter<position:
+                if counter == position - 1:
+                    new_element.next = n.next
+                    n.next = new_element
+                n = n.next
+                counter += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
     
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        #pass
+        n = self.head
+        previous = None
+        while n.value != value and n.next:
+            previous = n
+            n = n.next
+        if n.value == value:
+            if previous:
+                previous.next = n.next
+            else:
+                self.head = n.next
